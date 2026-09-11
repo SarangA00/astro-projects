f2b5de29d62b172d2adac38afaa6208d251318e5
SarangA00 <sarang.athani13@gmail.com>
2026-09-11 11:18:01 -0400

    Day 2: Basic escape velocity calculator

---
+++ escape_velocity.py
@@ -0,0 +1,16 @@
+import math
+
+# Initializing constants
+G = 6.67430e-11
+
+# Getting user data
+mass = float(input("Enter the planetary mass (kg): "))
+radius = float(input("Enter the planetary radius (km)): "))
+
+# Main calculation function
+def calc_escape_velocity(mass, radius):
+    vel = math.sqrt((2*G*mass)/(radius*1000))
+    adjVel = round((vel/1000), 2)
+    print("The escape velocity for the planet is: " + str(adjVel) + " km/sec.")
+
+calc_escape_velocity(mass, radius)
